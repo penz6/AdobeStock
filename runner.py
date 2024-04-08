c1de25afb0c50331e2fade5b7e993743d69e9d39
pennpotter <thetechkid66@gmail.com>
2024-04-07 22:39:37 -0400

    fixed api updated runner.py to write to file

--- runner.py
+++ runner.py
@@ -15,17 +15,8 @@ def main():
     #get data into data frame
     df = pd.json_normalize(data['files'],max_level=0)
     print(df)
-    #train/test data
-    X_train, X_test, y_train, y_test = train_test_split(df[['premium_level_id']], df['title'], test_size=.2)
-    classifier = KNeighborsClassifier(n_neighbors=3)
-    classifier.fit(X_train, y_train)
-    prediction = classifier.predict(X_test)
-    # Count how many were correctly classified
-    correct = np.where(prediction==y_test, 1, 0).sum()
-    print(correct)
-    # Calculate the accuracy of this classifier
-    accuracy = correct/len(y_test)
-    print(accuracy)
+    # save data to file
+    pd.DataFrame.to_csv(df,"Traning")
 
 
 
